60c68e020630fe7e282c7f14d6c0a7d593390de2
Marcelo Cepeda <marcelo.cepeda17@gmail.com>
2023-08-29 18:35:54 -0300

    Solucion ej 1

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -9,6 +9,11 @@ def maximo_basico(a: float, b: float) -> float:
         - No utilizar ELSE
         - No utilizar la función max
     """
+    if a > b:
+        return a
+    if b > a:
+        return b
+    return a
 
 
 # NO MODIFICAR - INICIO
@@ -24,6 +29,7 @@ def maximo_libreria(a: float, b: float) -> float:
     """Re-escribir utilizando el built-in max.
     Referencia: https://docs.python.org/3/library/functions.html#max
     """
+    return max(a,b)
 
 
 # NO MODIFICAR - INICIO
@@ -37,8 +43,10 @@ assert maximo_libreria(9, 18) == 18
 
 def maximo_ternario(a: float, b: float) -> float:
     """Re-escribir utilizando el operador ternario.
-    Referencia: https://docs.python.org/3/reference/expressions.html#conditional-expressions # noqa: E501
+    Referencia: https://docs.python.org/3/reference/expressions.html#conditional-expressions#noqa:E501
     """
+    return a if a > b else b 
+
 
 
 # NO MODIFICAR - INICIO
